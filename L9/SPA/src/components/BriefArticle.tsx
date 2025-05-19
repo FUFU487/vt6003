356d2902d4c590f410f9d84d5a7b22afbf87d193
vincent <118966490+FUFU487@users.noreply.github.com>
2025-05-19 19:59:11 +0800

    Lab9

---
+++ L9/SPA/src/components/BriefArticle.tsx
@@ -0,0 +1,17 @@
+import 'antd/dist/reset.css';
+import { Card} from 'antd';
+
+
+const BriefArticle = () => {
+  return (
+    <>
+      <Card style={{width: 300}} cover={<img alt="example" src="https://images.pexels.com/photos/1108701/pexels-photo-1108701.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />}
+  >
+        <h2>FUJI</h2>
+        
+      </Card>
+    </>
+  )
+}
+
+export default BriefArticle;
